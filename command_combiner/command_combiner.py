f424c3fd814555fc317511e801d9c849856ab4ff
Phanabani <phanabani@gmail.com>
2021-09-27 04:20:45 -0400

    Move into submodule and clean up.

--- command_combiner/command_combiner.py
+++ command_combiner/command_combiner.py
@@ -1,104 +1,15 @@
-from abc import ABCMeta, abstractmethod
 from collections.abc import Generator
 import re
 
-# This lookahead thing using tmp is an emulation of an atomic group
 from typing import NoReturn, Optional
 
+from nbt_encoder import NBTEncoder
+
 COMMAND_BLOCK_TEXT_LIMIT = 32500
+# This lookahead thing using tmp is an emulation of an atomic group
 command_pattern = re.compile(
     r'^(?=(?P<tmp>\s*))(?P=tmp)(?!#)\s*(?P<command>\S.*)$'
 )
-
-r'summon falling_block ~ ~5 ~ {BlockState:{Name:stone},Time:1b,'
-r'Passengers:[{id:\"minecraft:falling_block\",Time:0,'
-r'Passengers:[{id:\"minecraft:falling_block\",Time:1b,BlockState:{Name:redstone_block},'
-r'Passengers:[{id:\"minecraft:falling_block\",Time:0,'
-r'Passengers:[{id:\"minecraft:falling_block\",BlockState:{Name:activator_rail},Time:1b,'
-r'Passengers:[{id:command_block_minecart, Command:}]'
-
-
-class NBTNode(metaclass=ABCMeta):
-
-    @abstractmethod
-    def encode(self) -> str:
-        pass
-
-
-class RawNBT(NBTNode):
-
-    def __init__(self, text: str):
-        self.text = text
-
-    def encode(self):
-        return self.text
-
-
-class NBTEncoder:
-
-    def __init__(self, quote_strings: bool = True):
-        self.quote_strings = quote_strings
-
-    def encode(self, obj):
-        if isinstance(obj, NBTNode):
-            return obj.encode()
-
-        if isinstance(obj, dict):
-            return self.encode_dict(obj)
-
-        if isinstance(obj, list):
-            return self.encode_list(obj)
-
-        if isinstance(obj, str):
-            return self.encode_str(obj)
-
-        if isinstance(obj, float):
-            return self.encode_float(obj)
-
-        if isinstance(obj, bool):
-            # bools are also ints, so do this before ints
-            return self.encode_bool(obj)
-
-        if isinstance(obj, int) and not isinstance(obj, bool):
-            return self.encode_int(obj)
-
-        if obj is None:
-            return self.encode_none()
-
-        raise ValueError(
-            f"Failed to match type of {obj} ({type(obj)}) to any NBT type"
-        )
-
-    def encode_dict(self, obj: dict) -> str:
-        items = ['{']
-        for k, v in obj.items():
-            items.extend([k, ':', self.encode(v), ','])
-        items[-1] = '}'
-        return ''.join(items)
-
-    def encode_list(self, obj: list) -> str:
-        items = ['[']
-        for i in obj:
-            items.extend([self.encode(i), ','])
-        items[-1] = ']'
-        return ''.join(items)
-
-    def encode_str(self, obj: str) -> str:
-        if self.quote_strings:
-            return repr(obj)
-        return obj
-
-    def encode_float(self, obj: float) -> str:
-        return f'{obj:.6}'
-
-    def encode_int(self, obj: int) -> str:
-        return str(obj)
-
-    def encode_bool(self, obj: bool) -> str:
-        return str(obj).lower()
-
-    def encode_none(self) -> str:
-        return 'null'
 
 
 class NBTUtils:
@@ -198,16 +109,25 @@ class CommandCombiner:
 
 if __name__ == '__main__':
     from pathlib import Path
+    import sys
 
-    parent = Path(__file__).parent
+    def main():
+        if len(sys.argv) != 2:
+            return
 
-    cmds = []
-    with open(parent / 'commands.txt', 'rt') as f:
-        for line in f.readlines():
-            match = command_pattern.match(line)
-            if match:
-                cmds.append(match['command'])
+        file = Path(sys.argv[1])
+        if not file.exists():
+            return
 
-    combiner = CommandCombiner(cmds)
-    for combined in combiner.combine():
-        print(combined, end='\n\n')
+        cmds = []
+        with file.open() as f:
+            for line in f.readlines():
+                match = command_pattern.match(line)
+                if match:
+                    cmds.append(match['command'])
+
+        combiner = CommandCombiner(cmds)
+        for combined in combiner.combine():
+            print(combined, end='\n\n')
+
+    main()
---
+++ command_combiner/nbt_encoder.py
@@ -0,0 +1,88 @@
+from abc import ABCMeta, abstractmethod
+
+__all__ = [
+    'NBTNode', 'RawNBT', 'NBTEncoder'
+]
+
+
+class NBTNode(metaclass=ABCMeta):
+
+    @abstractmethod
+    def encode(self) -> str:
+        pass
+
+
+class RawNBT(NBTNode):
+
+    def __init__(self, text: str):
+        self.text = text
+
+    def encode(self):
+        return self.text
+
+
+class NBTEncoder:
+
+    def __init__(self, quote_strings: bool = True):
+        self.quote_strings = quote_strings
+
+    def encode(self, obj):
+        if isinstance(obj, NBTNode):
+            return obj.encode()
+
+        if isinstance(obj, dict):
+            return self.encode_dict(obj)
+
+        if isinstance(obj, list):
+            return self.encode_list(obj)
+
+        if isinstance(obj, str):
+            return self.encode_str(obj)
+
+        if isinstance(obj, float):
+            return self.encode_float(obj)
+
+        if isinstance(obj, bool):
+            # bools are also ints, so do this before ints
+            return self.encode_bool(obj)
+
+        if isinstance(obj, int) and not isinstance(obj, bool):
+            return self.encode_int(obj)
+
+        if obj is None:
+            return self.encode_none()
+
+        raise ValueError(
+            f"Failed to match type of {obj} ({type(obj)}) to any NBT type"
+        )
+
+    def encode_dict(self, obj: dict) -> str:
+        items = ['{']
+        for k, v in obj.items():
+            items.extend([k, ':', self.encode(v), ','])
+        items[-1] = '}'
+        return ''.join(items)
+
+    def encode_list(self, obj: list) -> str:
+        items = ['[']
+        for i in obj:
+            items.extend([self.encode(i), ','])
+        items[-1] = ']'
+        return ''.join(items)
+
+    def encode_str(self, obj: str) -> str:
+        if self.quote_strings:
+            return repr(obj)
+        return obj
+
+    def encode_float(self, obj: float) -> str:
+        return f'{obj:.6}'
+
+    def encode_int(self, obj: int) -> str:
+        return str(obj)
+
+    def encode_bool(self, obj: bool) -> str:
+        return str(obj).lower()
+
+    def encode_none(self) -> str:
+        return 'null'
